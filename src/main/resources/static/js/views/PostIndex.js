57209ef362c7ece698bb06eb6a1629912e438a7c
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-05-19 17:29:08 -0500

    added buttons for user to edit and delete

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -6,17 +6,19 @@ export default function PostIndex(props) {
         <main>
 <!--        code below is for adding new elements such as the post's content-->
             <div id="posts-container">
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
+                ${props.posts.map(post =>
+                    `<h3 id="content-title - ${post.id}">${post.title}</h3>
+                    <p id="content - ${post.content}"</p>
+                    <button type="enter" class="edit-button" data-id="${post.id}"></button>
+                    <button type="enter" class="delete-button" data-id="${post.id}"></button>
+
+                    `).join('')}   
             </div>
             <div id="new-post-form" class="new-post-form">
             <div type="text" id="new-post-title" class="new-post-title" placeholder="Post Title Here"
             </div>
             
-            <br>
-            
-            <div>
-            
 </div>
         </main>
     `;
-}
+    }
